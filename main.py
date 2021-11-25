e15e55a44032d55ab563d1840a4fc14a47f10c6e
ThanhLan <manondoublier@yahoo.fr>
2021-11-25 16:59:23 +0100

    fix heroku bug and add and cheatbox for actor

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import streamlit as st
 import pandas as pd
 
-#structure page
-st.title("Mon moteur de recherche de films et séries")
+#iniatlise page and data
+st.title("Hello, I am Harshad... ")
+st.write( " and I help you to find a movie or a tvshows for tonight")
 
-data = pd.read_csv('csv/movies.csv')
- 
+moviesdata = pd.read_csv('csv/movies.csv')
+tvshowdata = pd.read_csv('csv/tvshows.csv')
 
-st.write(data)
+def choose_movie_or_tvshow ():
+    """ Add a sidebar on the left for select movies of tvshows"""
+    add_selectbox = st.sidebar.selectbox(
+    "Do you want see...?",
+    ("A movie", "A Tv show")
+    )
+    if add_selectbox == "A movie":
+        st.write("This is the top 250 of movies by IMDB:")
+        st.write(moviesdata)
+    else:
+        st.write(tvshowdata)
+
+choose_movie_or_tvshow()
+
+st.sidebar.selectbox(
+"Have you... a favorite actor or actrice?",
+pd.Series
+ )
+
+
+
