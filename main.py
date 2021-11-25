d98d26959d8e12c8459097cd66cb33ee9a039d81
ThanhLan <manondoublier@yahoo.fr>
2021-11-25 19:48:57 +0100

    motif main

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import pandas as pd
 #structure page
 st.title("Mon moteur de recherche de films et séries")
 
-data = pd.read_csv('csv/movies.csv')
+datamovies = pd.read_csv('csv/movies.csv')
+
  
 
 st.write(data)
