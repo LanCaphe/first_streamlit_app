f37a9ca37f5ee92feacc9911434e6aff52cbbef7
ThanhLan <manondoublier@yahoo.fr>
2021-11-27 20:20:58 +0100

    edit main

--- main.py
+++ main.py
@@ -8,24 +8,28 @@ st.write( " and I help you to find a movie or a tvshows for tonight")
 moviesdata = pd.read_csv('csv/movies.csv')
 tvshowdata = pd.read_csv('csv/tvshows.csv')
 
-def choose_movie_or_tvshow ():
-    """ Add a sidebar on the left for select movies of tvshows"""
-    add_selectbox = st.sidebar.selectbox(
-    "Do you want see...?",
-    ("A movie", "A Tv show")
-    )
-    if add_selectbox == "A movie":
-        st.write("This is the top 250 of movies by IMDB:")
-        st.write(moviesdata)
-    else:
-        st.write(tvshowdata)
+data_filtered = st.container()
 
-choose_movie_or_tvshow()
+with data_filtered :
+  mask_to_display = df['my_col'] == my_var
+  st.dataframe(data=df[mask_to_display])
+
+
+Add a sidebar on the left for select movies of tvshows"""
+add_selectbox = st.sidebar.selectbox(
+#     "Do you want see...?",
+#     ("A movie", "A Tv show")
+#     )
+#     if add_selectbox == "A movie":
+#         st.write("This is the top 250 of movies by IMDB:")
+#         st.write(moviesdata)
+#     else:
+#         st.write(tvshowdata)
+
+# choose_movie_or_tvshow()
 
 add_actor_box= st.sidebar.selectbox(
 "Have you... a favorite actor or actrice?",
-("Ryan Gosling", "Emma Stone")
+("Ryan Gosling", "Emma Stone")                      
  )
-
-
 
